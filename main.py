f4f8649a1bb7a7557cbe912fde156790fd1f57a9
shaldybayev <shaldybayev.ch@gmail.com>
2025-10-23 21:58:41 +0500

    Add timestamp message to main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 def main():
-    message = "Hello, name!"
+    today = datetime.now().strftime("%Y-%m-%d %H:%M%S")
+    message = f"Today's Python session started at {today}!"
     print(message)
 
 if __name__ == "__main__":
