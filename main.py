c335be6709279b3c867f09675aed681065d95d87
shaldybayev <shaldybayev.ch@gmail.com>
2025-10-27 22:31:51 +0500

    daily commit

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-from datetime import date
-
-def motivation():
-    today = date.today()
-    print(f"Keep coding, my friend - {today} is a great day to learn Python!")
-
-if __name__ == "__main__":
-    motivation()
+name = input("What is your name? ")
+print(f"Hello, {name}! Welcome to Python!")
